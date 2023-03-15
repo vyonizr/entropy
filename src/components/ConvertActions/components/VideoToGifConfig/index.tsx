3e476989757972e779b7507a2d633eae83eb3bfe
Fitrahtur Rahman <vyonizr@gmail.com>
2023-03-15 11:20:56 +0700

    fix missing semicolon on GIF conversion

--- src/components/ConvertActions/components/VideoToGifConfig/index.tsx
+++ src/components/ConvertActions/components/VideoToGifConfig/index.tsx
@@ -22,7 +22,7 @@ export default function VideoToGifConfig({ file }: VideoToGifConfigProps) {
           selectedFile.name,
           outputExtension
         )
-        const method = `-vf fps=10,scale=-2:360:flags=lanczos,split[s0][s1][s0]palettegen[p][s1][p]paletteuse -loop 0`
+        const method = `-vf fps=10,scale=360:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse -loop 0`
         const outputData = await runFFMPEG(selectedFile, outputName, method)
         if (outputData) {
           const blob = new Blob([outputData.buffer], {
